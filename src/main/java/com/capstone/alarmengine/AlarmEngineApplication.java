9ac36356c2459694d1fa5ad2f6441d908cc3bc1c
Agnique <ccaizi@icloud.com>
2020-10-05 19:56:38 -0500

    initial commit

---
+++ src/main/java/com/capstone/alarmengine/AlarmEngineApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.alarmengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmEngineApplication.class, args);
+	}
+
+}
