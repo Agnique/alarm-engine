79b6653ca82249806278ad30ac036655df7d3d47
Agnique <ccaizi@icloud.com>
2020-11-11 00:22:06 -0600

    bug fix: CORS

--- src/main/java/com/capstone/alarmengine/control/FileController.java
+++ src/main/java/com/capstone/alarmengine/control/FileController.java
@@ -3,14 +3,12 @@ package com.capstone.alarmengine.control;
 import com.capstone.alarmengine.service.DeviceService;
 import com.capstone.alarmengine.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/file")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FileController {
 
     @Autowired
